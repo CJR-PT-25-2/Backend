4403146eb9fe78f9d51e968196e87fbbda2e70a2
pedrolrm <p.lucarocha@gmail.com>
2025-10-24 16:11:47 -0300

    feat(user): Cria a validação dos dados (DTO)

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'Preencha o campo E-mail' })
+  @IsEmail({}, { message: 'Enderaço de E-mail inválido' })
+  email: string;
+
+  @IsOptional()
+  @IsString({ message: 'Nome digitado inválido' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Preencha o campo Senha' })
+  @MinLength(8, { message: 'A senha deve conter no mínimo 8 caracteres' })
+  senha: string;
+
+  @IsOptional()
+  @IsUrl({}, { message: 'A foto fornecida deve ser uma URL válida' })
+  foto__perfil_URL: string;
+}
