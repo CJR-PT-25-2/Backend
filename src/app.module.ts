927083a3c0afd82a6c2e6fe72f07408f87ad6ebd
pedrolrm <p.lucarocha@gmail.com>
2025-10-30 22:57:33 -0300

    feat: cria a estrutura do módulo de autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { prismaModule } from 'prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, prismaModule],
+  imports: [UserModule, prismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
