eb92d4b468db42f4a4d1b8b2c995cc4028502591
pedrolrm <p.lucarocha@gmail.com>
2025-10-30 22:11:39 -0300

    fix: correção de sintaxe no método create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,30 +6,28 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {} 
+  constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-      const salt =10;
-      const senhaHash = await bcrypt.hash(createUserDto.senha,salt);
-      const user = await this.prisma.usuario.create({
-        data: { ...createUserDto }, 
-        senha: senhaHash,
-      })
+    const salt = 10;
+    const senhaHash = await bcrypt.hash(createUserDto.senha, salt);
+    const user = await this.prisma.usuario.create({
+      data: { ...createUserDto, senha: senhaHash },
+    });
     return user;
   }
 
- async findAll() {
+  async findAll() {
     return await this.prisma.usuario.findMany();
-    
   }
 
   async findOne(id: number) {
-    if(!id){
-            throw new Error('Usuario não encontrado');
-        }
-        return await this.prisma.usuario.findUnique({
-            where: { id },
-        });
+    if (!id) {
+      throw new Error('Usuario não encontrado');
+    }
+    return await this.prisma.usuario.findUnique({
+      where: { id },
+    });
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
