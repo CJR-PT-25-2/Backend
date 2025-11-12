7166ff3eb9b728a514d44737ebc4196bddf64d00
pedrolrm <p.lucarocha@gmail.com>
2025-11-12 17:36:21 -0300

    feat: implementação da validação de entradas e updates de  dados nas dtos

--- src/avaliacao-loja/dto/update-avaliacao-loja.dto.ts
+++ src/avaliacao-loja/dto/update-avaliacao-loja.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAvaliacaoLojaDto } from './create-avaliacao-loja.dto';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
-export class UpdateAvaliacaoLojaDto extends PartialType(CreateAvaliacaoLojaDto) {}
+export class UpdateAvaliacaoLojaDto extends PartialType(
+  CreateAvaliacaoLojaDto,
+) {
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  @IsOptional()
+  nota?: number;
+
+  @IsString()
+  @IsOptional()
+  comentario?: string;
+
+  loja_id: never;
+}
